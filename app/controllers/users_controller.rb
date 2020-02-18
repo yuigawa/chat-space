b73fff5a2cbd70f80387cce2e9007ca1ec7c3e82
yuigawa <57977700+yuigawa@users.noreply.github.com>
2020-02-18 14:21:35 +0900

    commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
 
-  def edit
+
+  def index
+    return nil if params[:keyword] == ""
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(id: current_user.id)
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
   def update
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,5 +5,6 @@ FactoryBot.define do
     email {Faker::Internet.free_email}
     password {password}
     password_confirmation {password}
+    
   end
 end
