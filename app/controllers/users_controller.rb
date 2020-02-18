c0fb7c5fc48e2bec53a4535a05a0040b92f4fe35
yuigawa <57977700+yuigawa@users.noreply.github.com>
2020-02-18 14:05:48 +0900

    create pull request

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,15 +10,6 @@ class UsersController < ApplicationController
     end
   end
 
-  def index
-    return nil if params[:keyword] == ""
-    @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
-    respond_to do |format|
-      format.html
-      format.json
-    end
-  end
-
   def update
     if current_user.update(user_params)
       redirect_to root_path
